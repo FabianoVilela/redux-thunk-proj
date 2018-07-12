0ba29e4cbaec2bf1d49335ab45178c189f5f1571
Fabiano Vilela <fabiano.vilela@gmail.com>
2018-07-11 22:33:39 -0300

    Preparing the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+
+import { reducer}  from './reducer'
+import Info from './Info'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
-    );
+      <Provider store={store}>
+        <div className="App">
+          <Info />
+        </div>
+      </Provider>
+    )
   }
 }
 
-export default App;
+export default App
